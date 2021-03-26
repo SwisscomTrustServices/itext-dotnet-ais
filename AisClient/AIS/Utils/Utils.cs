95fe47aa9012ecb77a59c97aa1a380225197ed6c
ESP\gellert.abraham <gellert.abraham@iquestgroup.com>
2021-03-26 15:51:53 +0200

    Implemented skeleton of CLI, added logic to parse command line arguments, display messages

---
+++ AisClient/AIS/Utils/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace AIS.Utils
+{
+    public class Utils
+    {
+        public static void CopyFileFromClasspathToDisk(string sourceFile, string destFile)
+        {
+            try
+            {
+                File.Copy(sourceFile, destFile, false);
+
+            } catch (IOException e)
+            {
+                // throw new AisClientException("Failed to create the file: [" + outputFile + "]");
+            }
+        }
+
+        public static string CopyFileFromClasspathToString(string inputFile)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(Path.Combine(Environment.CurrentDirectory, inputFile));
+                string text = reader.ReadToEnd();
+                reader.Close();
+                return text;
+
+            } catch (IOException e)
+            {
+                // throw new AisClientException("Failed to copy the file: [" + inputFile + "] to string");
+            }
+
+            return null;
+        }
+    }
+}
